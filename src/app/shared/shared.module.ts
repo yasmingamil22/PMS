7d031c380f18972fb7b08d55631d6e84177e563a
shrouk <shrouk@yahoo.com>
2024-05-13 00:07:07 +0300

    save

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,17 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { SharedRoutingModule } from './shared-routing.module';
 import { SharedComponent } from './shared.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RxReactiveFormsModule } from '@rxweb/reactive-form-validators';
-
 @NgModule({
   declarations: [
     SharedComponent,
@@ -27,7 +25,7 @@ import { RxReactiveFormsModule } from '@rxweb/reactive-form-validators';
     MatFormFieldModule,
     ReactiveFormsModule,
     RxReactiveFormsModule
-    
+
   ],
   exports: [
     SharedComponent,
